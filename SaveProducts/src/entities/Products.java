eaa34bc14c0b1adffee65b375120f0a52deff089
IIRINEU <IIRINEU@ford.com>
2022-02-27 18:09:31 -0300

    feat(save-products): entity Products implemented

---
+++ SaveProducts/src/entities/Products.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Products {
+
+    String name;
+    Double price;
+    Integer amount;
+
+    public Products() {}
+
+    public Products(String name, Double price, Integer amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
+
